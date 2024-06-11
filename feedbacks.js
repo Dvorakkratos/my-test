ed7a15924f15bfa99e3ad038e218660025dcfd56
Dvorakkratos <76841790+Dvorakkratos@users.noreply.github.com>
2024-06-11 14:20:53 +0200

    test 4

--- feedbacks.js
+++ feedbacks.js
@@ -1,13 +1,13 @@
 const { combineRgb } = require('@companion-module/base')
 
-module.exports = async function (self) {
+module.exports = async function(self) {
 	self.setFeedbackDefinitions({
 		ChannelState: {
 			name: 'Example Feedback',
 			type: 'boolean',
 			label: 'Channel State',
 			defaultStyle: {
-				bgcolor: combineRgb(255, 0, 0),
+				bgcolor: combineRgb(255, 255, 0),
 				color: combineRgb(0, 0, 0),
 			},
 			options: [
@@ -15,19 +15,38 @@ module.exports = async function (self) {
 					id: 'num',
 					type: 'number',
 					label: 'Test',
-					default: 5,
+					default: 6,
 					min: 0,
 					max: 10,
 				},
 			],
 			callback: (feedback) => {
-				console.log('Hello world!', feedback.options.num)
 				if (feedback.options.num > 5) {
 					return true
 				} else {
 					return false
 				}
 			},
+		},
+
+		test: {
+			name: 'TEST FEEDBACK',
+			type: 'boolean',
+			label: 'test label',
+
+			options: [
+				{
+					id: 'variable1',
+					type: 'number',
+					label: 'myVar',
+					default: 9,
+					min: 0,
+					max: 20,
+				},
+			],
+			callback: (feedback) => {
+				feedback.option.variable1 = 10;
+			}
 		},
 	})
 }
